c33506694b570089c19404304a04ad684db20a77
Alejandro Rayas <alexrayas@rocketmail.com>
2019-09-16 07:58:38 -0700

    First Commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    value: this.props.value,
+    tags: []
+  };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  handleIncrement = product => {
+    // console.log("Increment Clicked", this);
+    console.log(product);
+    this.setState({ value: this.state.value + 1 });
+  };
+
+  doHandleIncrement = () => {
+    this.handleIncrement({ id: 1 });
+  };
+
+  render() {
+    //console.log("props", this.props);
+
+    return (
+      <div>
+        {this.props.children}
+        {/* {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()} */}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement()}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {/* <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul> */}
+      </div>
+    );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value: count } = this.state;
+    const x = <h1>Zero</h1>;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
